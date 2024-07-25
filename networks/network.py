fbf04841d1d91e441782a5913951124e73ca733f
박한벗 <59501945+hanbuck30@users.noreply.github.com>
2024-07-25 11:09:24 +0900

    Add files via upload

--- networks/network.py
+++ networks/network.py
@@ -33,18 +33,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-import torch
-import torch.nn as nn
-from abc import ABCMeta
-
 class HebbianActor(nn.Module, metaclass=ABCMeta):
-    def __init__(self, layer_num, input_dim, output_dim, hidden_dim, activation_function=torch.tanh, last_activation=None, trainable_std=False, eta=0.0001):
+    def __init__(self, layer_num, input_dim, output_dim, hidden_dim, activation_function=torch.tanh, last_activation=None, trainable_std=False, eta=1e-6):
         super(HebbianActor, self).__init__()
         self.trainable_std = trainable_std
         self.activation = activation_function
         self.last_activation = last_activation
         self.eta = eta
-        self.clip = 0.01
+        self.weight_decay = 0.01
         layers_unit = [input_dim] + [hidden_dim] * (layer_num - 1)
         layers = [nn.Linear(layers_unit[idx], layers_unit[idx + 1]) for idx in range(len(layers_unit) - 1)]
         self.layers = nn.ModuleList(layers)
@@ -53,6 +49,15 @@ class HebbianActor(nn.Module, metaclass=ABCMeta):
             self.logstd = nn.Parameter(torch.zeros(1, output_dim))
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.device = device
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for layer in self.layers:
+            if isinstance(layer, nn.Linear):
+                nn.init.xavier_uniform_(layer.weight)
+        nn.init.xavier_uniform_(self.last_layer.weight)        
+        
+
     def forward(self, x):
         for layer in self.layers:
             x = self.activation(layer(x))
@@ -73,16 +78,18 @@ class HebbianActor(nn.Module, metaclass=ABCMeta):
                 pre_synaptic = x
                 x = self.activation(layer(x))
                 post_synaptic = x
-                layer.weight.data += torch.clamp(self.eta * torch.mm(pre_synaptic.T, (post_synaptic)).T, -self.clip, self.clip)
+                layer.weight.data *= (1-self.weight_decay)
+                layer.weight.data += self.eta * torch.mm(pre_synaptic.T, (post_synaptic)).T
         pre_synaptic = x
         post_synaptic = self.last_layer(x)
         if self.last_activation is not None:
             post_synaptic = self.last_activation(post_synaptic)
-        self.last_layer.weight.data  += torch.clamp(self.eta * torch.mm(pre_synaptic.T, delta_mu).T, -self.clip, self.clip)
+        self.last_layer.weight.data *= (1-self.weight_decay)
+        self.last_layer.weight.data  += self.eta * torch.mm(pre_synaptic.T, delta_mu).T
         return 
 
 class HebbianCritic(nn.Module, metaclass=ABCMeta):
-    def __init__(self, layer_num, input_dim, output_dim, hidden_dim, activation_function, last_activation=None, eta=0.0001):
+    def __init__(self, layer_num, input_dim, output_dim, hidden_dim, activation_function, last_activation=None, eta=1e-6):
         super(HebbianCritic, self).__init__()
         self.activation = activation_function
         self.last_activation = last_activation
@@ -94,7 +101,13 @@ class HebbianCritic(nn.Module, metaclass=ABCMeta):
         self.last_layer = nn.Linear(layers_unit[-1], output_dim)
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.device = device
-        
+        self._initialize_weights()
+
+    def _initialize_weights(self):
+        for layer in self.layers:
+            if isinstance(layer, nn.Linear):
+                nn.init.xavier_uniform_(layer.weight)
+        nn.init.xavier_uniform_(self.last_layer.weight)        
     def forward(self, x):
         for layer in self.layers:
             x = self.activation(layer(x))
@@ -104,20 +117,18 @@ class HebbianCritic(nn.Module, metaclass=ABCMeta):
         return x
 
     def hebbian_update(self, inputs, delta_v):
-        self.hebbian_weights = [torch.zeros_like(layer.weight) for layer in self.layers if isinstance(layer, nn.Linear)]
-        self.hebbian_weights.append(torch.zeros_like(self.last_layer.weight))
         x = inputs
         for i, layer in enumerate(self.layers):
             if isinstance(layer, nn.Linear):
                 pre_synaptic = x
                 x = self.activation(layer(x))
                 post_synaptic = x
-                self.hebbian_weights[i] = self.hebbian_weights[i].to(self.device)
-                self.hebbian_weights[i] += torch.clamp(self.eta * torch.mm(pre_synaptic.T, (post_synaptic)).T, self.clip, -self.clip)
-                layer.weight.data += self.hebbian_weights[i]
+                layer.weight.data *= (1-self.weight_decay)
+                layer.weight.data += self.eta * torch.mm(pre_synaptic.T, (post_synaptic)).T
         pre_synaptic = x
         post_synaptic = self.last_layer(x)
         if self.last_activation is not None:
             post_synaptic = self.last_activation(post_synaptic)
-        self.last_layer.weight.data += torch.clamp(self.eta * torch.mm(pre_synaptic.T, (delta_v)).T, self.clip, -self.clip)
+        self.last_layer.weight.data *= (1-self.weight_decay)
+        self.last_layer.weight.data  += self.eta * torch.mm(pre_synaptic.T, delta_v).T
         return 
